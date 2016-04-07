3e731b26a9084a4c302164f5ded8f7582471b9b1
Lakshman Peethani <lpeethani@allianceglobalservices.com>
2016-04-07 08:14:34 +0530

    services changes.

--- services.ts
+++ services.ts
@@ -1,11 +1,13 @@
 /// <reference path="typings/tsd.d.ts" />
 
 var https = require('https');
-//var httpClient = require('http');
-var exports:any = module.exports = {};
+var exports: any = module.exports = {};
 
 exports.GetLUISInfo = function(sourceText, callBack) {
-    var requestUri = `https://api.projectoxford.ai/luis/v1/application?id=07c4c72e-d229-4c7b-96db-2034c036d30e&subscription-key=c2ba4a70587642b7a4cada97a40584ed&q=${sourceText}`;
+    var luisBaseUrl = "https://api.projectoxford.ai/luis/v1";
+    var applicationId = "07c4c72e-d229-4c7b-96db-2034c036d30e";
+    var subscriptionKey = "c2ba4a70587642b7a4cada97a40584ed";
+    var requestUri = `${luisBaseUrl}/application?id=${applicationId}&subscription-key=${subscriptionKey}&q=${sourceText}`;
     https.get(requestUri, (response) => {
         if (callBack) {
             var pageData = '';
