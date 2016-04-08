847dfa2c160c2078e11ce2b4d8dcc37e9ab4db23
Lakshman Peethani <lpeethani@allianceglobalservices.com>
2016-04-08 13:24:21 +0530

    basic working bot

--- stockbot.js
+++ stockbot.js
@@ -25,10 +25,47 @@ botService.on('contactAdded', (bot, data) => {
 botService.on('message', (bot, data) => {
     console.log("message received ");
 });
-botService.on('personalMessage', (bot, data) => {
-    console.log("Message received \n" + JSON.stringify(data));
+botService.on('personalMessage', (bot, botdata) => {
+    //console.log("Message received \n" + JSON.stringify(data));
     //bot.reply(`Hey ${data.from}. Thank you for your message: "${data.content}".`, true);
-    if (data.content !== "") {
+    if (botdata.content !== "") {
+        var lclient = new luisclient.LUISClient();
+        lclient.GetLUISInfo(botdata.content, (error, data) => {
+            //console.log(data.intents);
+            var primaryIntent = lclient.getPrimaryIntent(data);
+            console.log(primaryIntent);
+            switch (primaryIntent.intent) {
+                case "Greeting":
+                    bot.reply(`Hey ${botdata.from}. `, true);
+                    break;
+                case "GetStockQuote":
+                    var sclient = new stockClient.StockClient();
+                    sclient.getStockSymbol(primaryIntent.actions[0].parameters[0].value[0].entity, (error, data) => {
+                        if (error)
+                            console.log(error);
+                        if (data) {
+                            //console.log(data);
+                            data.forEach((symbol) => {
+                                sclient.getStockPrice(symbol.Symbol, (error, data) => {
+                                    if (error)
+                                        console.log(error);
+                                    if (data) {
+                                        //console.log(data);
+                                        data.forEach((stockInfo) => {
+                                            console.log(`${stockInfo.t} : ${stockInfo.l}`);
+                                            bot.reply(`Here is the current stock value : "${stockInfo.t} ${stockInfo.l}" from ${stockInfo.e}.`, true);
+                                        });
+                                    }
+                                    ;
+                                });
+                            });
+                        }
+                    });
+                    break;
+                default:
+                    break;
+            }
+        });
     }
 });
 botService.on('groupMessage', (bot, message) => {
@@ -43,34 +80,7 @@ server.post('/v1/message', skype.messagingHandler(botService));
 server.post('/v1/call', function (data) {
     console.log("Call recevied" + data);
 });
-// const port = process.env.PORT || 8080;
-// server.listen(port);
-// console.log('Listening for incoming requests on port ' + port);
-var lclient = new luisclient.LUISClient();
-lclient.GetLUISInfo("get Ford stock price", (error, data) => {
-    //console.log(data.intents);
-    var primaryIntent = lclient.getPrimaryIntent(data);
-    console.log(primaryIntent.actions[0].parameters[0].value[0].entity);
-    var sclient = new stockClient.StockClient();
-    sclient.getStockSymbol(primaryIntent.actions[0].parameters[0].value[0].entity, (error, data) => {
-        if (error)
-            console.log(error);
-        if (data) {
-            //console.log(data);
-            data.forEach((symbol) => {
-                sclient.getStockPrice(symbol.Symbol, (error, data) => {
-                    if (error)
-                        console.log(error);
-                    if (data) {
-                        //console.log(data);
-                        data.forEach((stockInfo) => {
-                            console.log(`${stockInfo.t} : ${stockInfo.l}`);
-                        });
-                    }
-                    ;
-                });
-            });
-        }
-    });
-});
+const port = process.env.PORT || 8080;
+server.listen(port);
+console.log('Listening for incoming requests on port ' + port);
 //# sourceMappingURL=stockbot.js.map
--- stockbot.ts
+++ stockbot.ts
@@ -30,11 +30,49 @@ botService.on('message', (bot, data) => {
     console.log("message received ");
 });
 
-botService.on('personalMessage', (bot, data) => {
+botService.on('personalMessage', (bot, botdata) => {
 
-    console.log("Message received \n" + JSON.stringify(data));
+    //console.log("Message received \n" + JSON.stringify(data));
     //bot.reply(`Hey ${data.from}. Thank you for your message: "${data.content}".`, true);
-    if (data.content !== "") {
+    if (botdata.content !== "") {
+
+        var lclient = new luisclient.LUISClient();
+        lclient.GetLUISInfo(botdata.content, (error, data) => {
+            //console.log(data.intents);
+            var primaryIntent = lclient.getPrimaryIntent(data);
+            console.log(primaryIntent);
+            switch (primaryIntent.intent) {
+                case "Greeting":
+                        bot.reply(`Hey ${botdata.from}. `, true);
+                    break;
+                case "GetStockQuote":
+                    var sclient = new stockClient.StockClient()
+                    sclient.getStockSymbol(primaryIntent.actions[0].parameters[0].value[0].entity, (error, data) => {
+                        if (error) console.log(error);
+                        if (data) {
+                            //console.log(data);
+                            data.forEach((symbol) => {
+                                sclient.getStockPrice(symbol.Symbol, (error, data) => {
+                                    if (error) console.log(error);
+                                    if (data) {
+                                        //console.log(data);
+                                        data.forEach((stockInfo) => {
+                                            console.log(`${stockInfo.t} : ${stockInfo.l}`);
+                                            bot.reply(`Here is the current stock value : "${stockInfo.t} ${stockInfo.l}" from ${stockInfo.e}.`, true);
+                                        })
+                                    };
+                                });
+                            });
+                        }
+                    });
+                    break;
+
+                default:
+                    break;
+            }
+
+        })
+
         //stockClient.getStockSymbol('test', (error, data) => {});
         // http.get(`http://www.google.com/finance/info?q=${data.content}`, (res) => {
         //     console.log(`Got response: ${res.statusCode}`);
@@ -83,33 +121,6 @@ server.post('/v1/call', function(data) {
     console.log("Call recevied" + data)
 });
 
-// const port = process.env.PORT || 8080;
-// server.listen(port);
-// console.log('Listening for incoming requests on port ' + port);
-
-
-var lclient = new luisclient.LUISClient();
-lclient.GetLUISInfo("get Ford stock price", (error, data) => {
-    //console.log(data.intents);
-    var primaryIntent = lclient.getPrimaryIntent(data);
-    console.log(primaryIntent.actions[0].parameters[0].value[0].entity);
-
-    var sclient = new stockClient.StockClient()
-    sclient.getStockSymbol(primaryIntent.actions[0].parameters[0].value[0].entity, (error, data) => {
-        if (error) console.log(error);
-        if (data) {
-            //console.log(data);
-            data.forEach((symbol) => {
-                sclient.getStockPrice(symbol.Symbol, (error, data) => {
-                    if (error) console.log(error);
-                    if (data) {
-                        //console.log(data);
-                        data.forEach((stockInfo) => {
-                            console.log(`${stockInfo.t} : ${stockInfo.l}`);
-                        })
-                    };
-                });
-            });
-        }
-    });
-})
+const port = process.env.PORT || 8080;
+server.listen(port);
+console.log('Listening for incoming requests on port ' + port);
