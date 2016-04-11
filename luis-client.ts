ed4b3e29cd9c5d7540b62ab5d217f87bd6e827fa
Lakshman Peethani <lpeethani@allianceglobalservices.com>
2016-04-11 10:18:20 +0530

    luis preview api method added.

--- luis-client.ts
+++ luis-client.ts
@@ -7,6 +7,10 @@ interface LuisCallback {
     (erro: any, response: luis.LUISResponse)
 }
 
+interface LuisCallbackPreview {
+    (erro: any, response: luis.LUISPreviewResponse)
+}
+
 export class LUISClient {
 
     constructor() {
@@ -15,6 +19,30 @@ export class LUISClient {
 
     GetLUISInfo(sourceText: string, callback: LuisCallback) {
         var luisBaseUrl = "https://api.projectoxford.ai/luis/v1/application";
+        var applicationId = "07c4c72e-d229-4c7b-96db-2034c036d30e";
+        var subscriptionKey = "c2ba4a70587642b7a4cada97a40584ed";
+        var requestUri = `${luisBaseUrl}?id=${applicationId}&subscription-key=${subscriptionKey}&q=${sourceText}`;
+        fetch(requestUri).then((res) => {
+            if (res.status === 200) {
+                return res.json();
+            }
+            else {
+                callback(
+                    {
+                        url: res.url,
+                        status: res.status,
+                        statusText: res.statusText
+                    }, null);
+            }
+        }).then((data) => {
+            if (callback) {
+                callback(null, data)
+            }
+        });
+    }
+    
+    GetLUISInfoPreview(sourceText: string, callback: LuisCallbackPreview) {
+        var luisBaseUrl = "https://api.projectoxford.ai/luis/v1/application/preview";
         var applicationId = "07c4c72e-d229-4c7b-96db-2034c036d30e";
         var subscriptionKey = "c2ba4a70587642b7a4cada97a40584ed";
         //var requestUri = `${luisBaseUrl}/application?id=${applicationId}&subscription-key=${subscriptionKey}&q=${sourceText}`;
@@ -42,6 +70,10 @@ export class LUISClient {
         return luisInfo.intents.sort((a, b) => {
             return a.score < b.score ? 1 : -1;
         })[0];
+    }
+    
+    getPrimaryIntentPreview(luisInfo : luis.LUISPreviewResponse) {
+        return luisInfo.topScoringIntent;
     }
 }
 
